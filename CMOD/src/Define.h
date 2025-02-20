ea68f45fbc2aa31fa2a64b537458398da10fec2b
passyur <jacob.c.mcgrath@gmail.com>
2025-02-19 22:37:38 -0600

    Rm unused constants

--- CMOD/src/Define.h
+++ CMOD/src/Define.h
@@ -37,7 +37,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // Constants
 //#define RAND_MAX 2147483647.0L
 //#define HRAND_MAX 1073741824.0L
-#define NYQUIST 22050.0L
+// #define NYQUIST 22050.0L
 #define CEILING 15000.0L
 #define MINFREQ 20.0L
 #define PI 4.0*atan(1.0)
@@ -51,22 +51,22 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //static const char * INFILE = "daria";
 
 // general
-static const double GOLDENMEAN = 0.6182;
-static const float MIN_AUD_FREQ = 20.;
-static const float MAX_AUD_FREQ = 20000.;
+// static const double GOLDENMEAN = 0.6182;
+// static const float MIN_AUD_FREQ = 20.;
+// static const float MAX_AUD_FREQ = 20000.;
 static const float FUNDAMENTAL = 13.75;
 static const double C0 = 16.35159783;
 static const double WELL_TEMP_INCR = pow(2, 1./12.);   	
 //static const double WELL_TEMP_INCR = pow(2, 1./24.);
 //static const double WELL_TEMP_INCR = 1.0594631;
-static const float MAX_SONES = 256.;
+// static const float MAX_SONES = 256.;
 static const float FIRST_CONST = -5.54;
 static const float SECOND_CONST = -1.84;
 static const int SAMPLING_RATE = 44100;
 
 // constants for the Event class
-static const int DEFAULT_START_TIME = 0;
-static const int DEFAULT_DURATION = 1;
+// static const int DEFAULT_START_TIME = 0;
+// static const int DEFAULT_DURATION = 1;
 
 #endif /* DEFINE_H */
 
