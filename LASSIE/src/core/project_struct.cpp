4ea0ade2c1ca5ea1ac972813ebd01cbfbf5c774f
passyur <jacob.c.mcgrath@gmail.com>
2025-06-24 10:55:49 -0500

    (WIP) parser

--- LASSIE/src/core/project_struct.cpp
+++ LASSIE/src/core/project_struct.cpp
@@ -3,88 +3,265 @@ The 'project' object that keeps track of project details to deploy
 in the associated window (currently, the project view).
 */
 #include "project_struct.hpp"
+// #include "IEvent.h"
 
 #include "../mainwindow.hpp"
 
 #include <QFile>
+#include <QTextStream>
 #include <QFileInfo>
 #include <QtLogging>
-#include <xercesc/dom/DOMDocument.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/util/XMLUni.hpp>
+#include <xercesc/parsers/XercesDOMParser.hpp>
 
-// #include "muParser.h"
+namespace XercesParser {
+    using namespace xercesc;
+    std::string getFunctionString(DOMElement* _thisFunctionElement){
+        char* charBuffer;
 
-// QString modifiers[] = {
-//     "-8va", 
-//     "+8va",
-//     "accent",
-//     "espressivo",
-//     "marcato",
-//     "portato",
-//     "staccatissimo",
-//     "staccato",
-//     "tenuto",
-//     "prall",
-//     "prallup",
-//     "pralldown",
-//     "upprall",
-//     "downprall",
-//     "prallprall",
-//     "lineprall",
-//     "prallmordent",
-//     "mordent",
-//     "upmordent",
-//     "downmordent",
-//     "trill",
-//     "turn",
-//     "reverseturn",
-//     "shortfermata",
-//     "fermata",
-//     "longfermata",
-//     "verylongfermata",
-//     "upbow",
-//     "downbow",
-//     "flageolet",
-//     "open",
-//     "halfopen",
-//     "lheel",
-//     "rheel",
-//     "ltoe",
-//     "rtoe",
-//     "snappizzicato",
-//     "stopped",
-//     "segno",
-//     "coda",
-//     "varcoda",
-//     "null"
-// };
-  
-// namespace ProjectHelper {
-//     constexpr void initializeModifiers(std::map<QString, bool>& default_note_modifiers) {
-//         for(unsigned int i = 0; i < sizeof(modifiers) / sizeof(QString); ++i){
-//             default_note_modifiers.insert(pair<QString, bool>(modifiers[i], true));
-//         }
+        DOMCharacterData* textData;
+        string returnString;
+        DOMNode* child = _thisFunctionElement->getFirstChild();
+        if (child == NULL){ //not containing any child, return string
+            return "";
+        }
 
-//         std::map<QString, bool>::iterator iter = default_note_modifiers.begin();
-//         while (iter != default_note_modifiers.end()) {
-//             (*iter).second = true;
-//             iter++;
-//         }
-//     }
+        XMLCh tempStr[3] = {chLatin_L, chLatin_S, chNull};
+        DOMImplementation *impl          = DOMImplementationRegistry::getDOMImplementation(tempStr);
+        DOMLSSerializer   *theSerializer = ((DOMImplementationLS*)impl)->createLSSerializer();
+        XMLCh* bla = theSerializer->writeToString (_thisFunctionElement);
+        char* toTranscode = XMLString::transcode(bla);
+        returnString = string (toTranscode);
+        XMLString::release(&toTranscode);
+        XMLString::release(&bla);
 
-//     /* called before appearing (see immediately succeeding f'n) the checkboxes; init'd when a user brings up the configure note modifier menu */
-//     inline void initializeNoteModifierButton(std::map<QString, bool>& default_note_modifiers, GTK::CheckButton& checkbox, std::string key_name, std::string button_name){
-//         note_modifiers_configuration_dialog_ref_builder->get_widget(button_name, checkbox);
-//         /* let the compiler do this, branchless */
-//         checkbox->set_active(default_note_modifiers[key_name]);
-//     }
+        int tagLength = (int) XMLString::stringLen(_thisFunctionElement->getTagName());
 
-//     inline void appearNoteModifierButton(std::map<QString, bool>& default_note_modifiers, GTK::CheckButton& checkbox, std::string key_name, std::string button_name){
-//         note_modifiers_configuration_dialog_ref_builder->get_widget(button_name, checkbox);
-//         default_note_modifiers[key_name] = checkbox->get_active();
-//     }
-// }
+        delete theSerializer;
+        returnString = returnString.substr(tagLength+2, returnString.size() - tagLength * 2 - 5);
+        return returnString;
+    } 
+}
 
-unsigned int counter = 1;
+void ProjectManager::parse(Project *p, const QString& filepath){
+    using namespace xercesc;
+    XMLPlatformUtils::Initialize();
+    XercesDOMParser* parser = new XercesDOMParser();
+
+    std::string file_name = filepath.toStdString();
+    parser->parse(file_name.c_str());
+    DOMDocument *document = parser->getDocument();
+    DOMElement *root = document->getDocumentElement();
+
+    char *buffer;
+    DOMCharacterData *char_data;
+
+    //Configurations
+    DOMElement* configuration = root->getFirstElementChild();
+    
+    //fileFlag
+    DOMElement* element = configuration->getFirstElementChild();
+    element = element->getNextElementSibling(); // skip Title attribute
+    p->file_flag = QString::fromStdString(XercesParser::getFunctionString(element));
+    //topEvent
+    element = element->getNextElementSibling();
+    p->top_event = QString::fromStdString(XercesParser::getFunctionString(element));
+
+    // pieceStartTime
+    element = element->getNextElementSibling(); //skipped
+
+    //duration
+    element = element->getNextElementSibling();
+    p->duration = QString::fromStdString(XercesParser::getFunctionString(element));
+
+    //synthesis
+    element = element->getNextElementSibling();
+    if(XercesParser::getFunctionString(element) == "True")
+        p->synthesis = true;
+    else
+        p->synthesis = false;
+
+    //score
+    element = element->getNextElementSibling();
+    if(XercesParser::getFunctionString(element) == "True")
+        p->score = true;
+    else
+        p->score = false;
+
+    //grandstaff
+    element = element->getNextElementSibling();
+    if(XercesParser::getFunctionString(element) == "True")
+        p->grand_staff = true;
+    else
+        p->grand_staff = false;
+
+        //numOfStaff
+    element = element->getNextElementSibling();
+    p->num_staffs = QString::fromStdString(XercesParser::getFunctionString(element));
+
+    //numOfChannels
+    element = element->getNextElementSibling();
+    p->num_channels = QString::fromStdString(XercesParser::getFunctionString(element));
+
+    //sampleRate
+    element = element->getNextElementSibling();
+    p->sample_rate = QString::fromStdString(XercesParser::getFunctionString(element));
+
+    //sampleSize
+    element = element->getNextElementSibling();
+    p->sample_size = QString::fromStdString(XercesParser::getFunctionString(element));
+
+    //numOfThreads
+    element = element->getNextElementSibling();
+    p->num_threads = QString::fromStdString(XercesParser::getFunctionString(element));
+
+    // Output Particel
+    element = element->getNextElementSibling();
+    if(element != nullptr /* why this nullptr check? -6/23/25 */ && XercesParser::getFunctionString(element) == "True")
+        p->output_particel = true;
+    else
+        p->output_particel = false;
+    
+    DOMElement* noteModifiers = configuration->getNextElementSibling();
+    element = noteModifiers->getFirstElementChild(); //default modifiers
+    char_data = ( DOMCharacterData*) element->getFirstChild();
+    buffer = XMLString::transcode(char_data->getData());
+
+    // auto modifierMapIter = p->default_note_modifiers.begin();
+    // int index = 0;
+    // for(auto modifierMapIter = p->default_note_modifiers.begin(); modifierMapIter != p->default_note_modifiers.end(); ++modifierMapIter){
+    //     modifierMapIter.value() = true;
+    // //    (*modifierMapIter).second = (charBuffer[index]=='1')?true:false;
+    //     index = index + 3;
+    // }
+    XMLString::release(&buffer);
+
+    element = element->getNextElementSibling(); //custom modifiers
+    DOMElement* modifier = element->getFirstElementChild();
+
+    while (modifier != NULL){
+        char_data = (DOMCharacterData*)modifier->getFirstChild();
+        buffer = XMLString::transcode(char_data->getData());
+        p->custom_note_modifiers.append(QString::fromStdString(string(buffer)));
+        XMLString::release(&buffer);
+        modifier = modifier->getNextElementSibling();
+    }
+
+    DOMElement *envlibelement = noteModifiers->getNextElementSibling();
+    char_data = ( DOMCharacterData*) envlibelement->getFirstChild();
+
+    buffer = XMLString::transcode(char_data->getData());
+    std::string fileString = file_name + ".lib.temp";
+    QFile file(QString::fromStdString(fileString));
+    if(!file.open(QIODevice::WriteOnly | QIODevice::Text)){
+        return;
+    }
+
+    QTextStream out(&file);
+    out << buffer;
+    XMLString::release(&buffer);
+    file.close();
+
+    EnvelopeLibrary* envelopeLibrary = new EnvelopeLibrary();
+    envelopeLibrary->loadLibraryNewFormat((char*)fileString.c_str());
+    std::string deleteCommand = "rm " + fileString;
+    system(deleteCommand.c_str());
+
+    EnvelopeLibraryEntry* previousEntry = NULL;
+    Envelope* thisEnvelope;
+
+    for (int i = 1; i <= envelopeLibrary->size(); i ++){
+        thisEnvelope = envelopeLibrary->getEnvelope(i);
+        EnvelopeLibraryEntry* thisEntry = new EnvelopeLibraryEntry(thisEnvelope, i);
+        delete thisEnvelope;
+
+        if (previousEntry ==NULL){
+            p->elentry = thisEntry;
+            previousEntry = thisEntry;
+            thisEntry->prev = NULL;
+        }else{
+            previousEntry->next = thisEntry;
+            thisEntry->prev = previousEntry;
+        }
+        previousEntry = thisEntry;
+    }
+
+    delete envelopeLibrary;
+
+    DOMElement* currentElement = envlibelement;
+
+    DOMElement* markovModelLibraryElement = envlibelement->getNextElementSibling();
+    std::string tagName = buffer = XMLString::transcode(markovModelLibraryElement->getTagName());
+    XMLString::release(&buffer);
+    if (tagName == "MarkovModelLibrary") {
+        currentElement = markovModelLibraryElement;
+        DOMText* text = dynamic_cast<DOMText*>(markovModelLibraryElement->getFirstChild());
+        std::string data = buffer = XMLString::transcode(text->getWholeText());
+        XMLString::release(&buffer);
+
+        // QTextStream ts(QString::fromStdString(data));
+
+        // // read the number of models
+        // int size;
+        // ts >> size;
+        // p->markovModels.resize(size);
+
+        // // read individual models
+        // std::string modelText;
+        // QString line;
+        // getline(ts, line, '\n');
+        // for (int i = 0; i < size; i++) {
+        //     line = ts.readLine();
+        //     modelText = line + '\n';
+        //     for (int j = 0; j < 1; j++){
+        //         line = ts.readLine();
+        //         modelText += line + '\n';
+        //     }
+        //     line = ts.readLine();
+        //     modelText += line;
+        std::stringstream ss(data);
+
+        // read the number of models
+        long long size;
+        ss >> size;
+        p->markovModels.resize(size);
+
+        // read individual models
+        std::string modelText, line;
+        getline(ss, line, '\n');
+        for (int i = 0; i < size; i++) {
+            getline(ss, line, '\n');
+            modelText = line + '\n';
+            getline(ss, line, '\n');
+            modelText += line + '\n';
+            getline(ss, line, '\n');
+            modelText += line + '\n';
+            getline(ss, line, '\n');
+            modelText += line;
+
+            (p->markovModels[i])->from_str(modelText);
+        }
+    }
+
+    DOMElement *domEvents = currentElement->getNextElementSibling();
+    DOMElement *eventElement = domEvents->getFirstElementChild();
+
+    while (eventElement != NULL){
+        IEvent* newEvent = new IEvent(eventElement);
+        // paletteView->insertEvent(newEvent, newEvent->getEventTypeString());
+        p->events.push_back(newEvent);
+        eventElement = eventElement->getNextElementSibling();
+    }
+
+    auto eventsIter = p->events.begin();
+    for (; eventsIter != p->events.end(); ++eventsIter){
+        (*eventsIter)->link(p);
+    }
+}
+
+
 /** the empty constructor for a NEW project, will return a ProjectView  **/
     
     // filepath = "";
@@ -119,11 +296,11 @@ unsigned int counter = 1;
     // env_lib_entries = NULL;
 
 Project::Project(const QString& _title, const QByteArray& _id){
-    if(_title.isEmpty())
+    if(_title.isEmpty()){
         title = tr("Untitled");
-    else
+    }else{
         title = _title;
-    
+    }
 #ifdef TABEDITOR
     if(_id.isEmpty())
         id = QUuid::createUuid().toString().toLatin1();
@@ -161,6 +338,8 @@ Project* ProjectManager::open(const QString& filepath, const QByteArray& id){
 
     Project *project = create(info.fileName(), id);
     project->filepath = filepath;
+
+    parse(project, filepath);
     
     return project;
 }
