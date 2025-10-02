7ddea16f55feddec4b4e317de4eb4661b4066f1d
passyur <jacob.c.mcgrath@gmail.com>
2025-10-02 17:53:46 -0500

    reorg: move LayerBox into widget directory

--- LASSIE/src/windows/EventAttributesViewController.cpp
+++ LASSIE/src/windows/EventAttributesViewController.cpp
@@ -5,6 +5,7 @@
 #include "ProjectViewController.hpp"
 //#include "PaletteViewController.h"
 //#include "PartialWindow.h"
+#include "../widgets/LayerBox.hpp"
 
 #include <QMessageBox>
 #include <QKeyEvent>
@@ -163,66 +164,11 @@ void EventAttributesViewController::fixStackedWidgetLayout(QWidget* currPage) {
     ui->stackedWidget->adjustSize();
 }
 
-LayerBox::LayerBox(EventAttributesViewController* parentController,
-                   ProjectView* projectView
-                   //EventLayer* layerModel,
-                    // bool showDiscreteColumns
-                   )
-    : QFrame(nullptr),
-      m_attributesView(parentController),
-      m_projectView(projectView)
-     // m_layerModel(layerModel)
-{
-    m_mainLayout = new QVBoxLayout();
-    this->setLayout(m_mainLayout);
-    this->setFrameShape(QFrame::StyledPanel);
-    this->setLineWidth(1);
+void EventAttributesViewController::showAttributesOfEvent(Eventtype type, unsigned index) {
 
-    auto* weightHBox = new QHBoxLayout;
-    auto* weightLabel = new QLabel("Number of children in this layer:");
-    m_weightEntry = new QLineEdit;
-    m_weightFuncButton = new QPushButton("Insert Function");
-    m_deleteLayerButton = new QPushButton("Delete This Layer");
 
-    connect(m_weightFuncButton, &QPushButton::clicked,
-            this, &LayerBox::onWeightFunctionClicked);
-    connect(m_deleteLayerButton, &QPushButton::clicked,
-            this, &LayerBox::onDeleteLayerClicked);
-
-    weightHBox->addWidget(weightLabel);
-    weightHBox->addWidget(m_weightEntry);
-    weightHBox->addWidget(m_weightFuncButton);
-    weightHBox->addWidget(m_deleteLayerButton);
-
-    m_mainLayout->addLayout(weightHBox);
-
-    m_treeView = new QTreeView;
-    m_model = new QStandardItemModel;
-    m_model->setHorizontalHeaderLabels({"Child Type", "Class", "Name"});
-    m_treeView->setModel(m_model);
-    m_treeView->setSelectionBehavior(QAbstractItemView::SelectRows);
-    m_treeView->setEditTriggers(QAbstractItemView::NoEditTriggers);
-
-    m_mainLayout->addWidget(m_treeView);
-
-    this->setMinimumHeight(200);
-}
-
-LayerBox::~LayerBox() {}
-
-void EventAttributesViewController::showAttributesOfEvent(Eventtype type, unsigned index) { // (IEvent* event)
-    // if (!event) {
-    //     m_currentlyShownEvent = nullptr;
-    //     ui->stackedWidget->setCurrentWidget(ui->emptyPage);
-    //     // clear dynamic widgets
-    //     ui->layersLayout->deleteLater();
-    //     ui->modifiersLayout->deleteLater();
-    //     ui->partialsLayout->deleteLater();
-    //     return;
-    // }
-
+    /// \todo implemenet saveCurrentShownEventData();
     // saveCurrentShownEventData();
-    // m_currentlyShownEvent = event;
     m_curreventtype = type;
     m_curreventindex = index;
     
